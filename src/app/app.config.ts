a2e7d40227d4b4de0f1ad42815f2df19d0f280c6
nukun56788 <107339530+NukunK@users.noreply.github.com>
2025-05-07 11:46:00 +0700

    add all routes component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,6 +13,14 @@ import { MyBoothsComponent } from './my-booths/my-booths.component';
 import { BoothDetailsComponent } from './booth-details/booth-details.component';
 import { SettingsComponent } from './settings/settings.component';
 import { RegisterComponent } from './register/register.component';
+import { BookingListComponent } from './booking-list/booking-list.component';
+import { BoothBookingsComponent } from './booth-bookings/booth-bookings.component';
+import { BoothListadminComponent } from './booth-listadmin/booth-listadmin.component';
+import { DashboardadminComponent } from './dashboardadmin/dashboardadmin.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { PendingUsersComponent } from './pending-users/pending-users.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { ZoneListComponent } from './zone-list/zone-list.component';
 
 const routes = [
   { path: '', component: IndexComponent },
@@ -24,6 +32,16 @@ const routes = [
   { path: 'booth-details', component: BoothDetailsComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'register', component: RegisterComponent },
+
+
+  { path: 'bookings', component: BookingListComponent},
+  { path: 'booth-bookings', component: BoothBookingsComponent},
+  { path: 'booth-list/:zone_id/:zone_name', component:BoothListadminComponent},
+  { path: 'dashboardadmin', component:DashboardadminComponent},
+  { path: 'users', component:UserListComponent},
+  { path: 'zone-listamdin', component:ZoneListComponent},
+  { path: 'event-listadmin', component:EventListComponent},
+  { path: 'pending-users', component:PendingUsersComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' as 'full' } // ย้ายไว้ด้านล่างสุด และแก้ไข pathMatch
 ];
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,15 @@ import { MyBoothsComponent } from './my-booths/my-booths.component';
 import { BoothDetailsComponent } from './booth-details/booth-details.component';
 import { SettingsComponent } from './settings/settings.component';
 import { RegisterComponent } from './register/register.component';
+import { BoothListadminComponent } from './booth-listadmin/booth-listadmin.component';
+import { BoothBookingsComponent } from './booth-bookings/booth-bookings.component';
+import { BookingListComponent } from './booking-list/booking-list.component';
+import { DashboardadminComponent } from './dashboardadmin/dashboardadmin.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { ZoneListComponent } from './zone-list/zone-list.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { PendingUsersComponent } from './pending-users/pending-users.component';
+
 export const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'login', component: LoginComponent },
@@ -18,6 +27,16 @@ export const routes: Routes = [
   { path: 'booth-details', component: BoothDetailsComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'register', component: RegisterComponent },
+
+
+  { path: 'bookings', component: BookingListComponent},
+  { path: 'booth-bookings', component: BoothBookingsComponent},
+  { path: 'booth-list/:zone_id/:zone_name', component:BoothListadminComponent},
+  { path: 'dashboardadmin', component:DashboardadminComponent},
+  { path: 'users', component:UserListComponent},
+  { path: 'zone-listamdin', component:ZoneListComponent},
+  { path: 'event-listadmin', component:EventListComponent},
+  { path: 'pending-users', component:PendingUsersComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' } // ย้ายไว้ด้านล่างสุด
 ];
 
